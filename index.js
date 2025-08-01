db3b8f0b89e307b413864a9c90eedec9a76fd94e
mfhrahat1165 Name <rahat24122004@gmail.com>
2025-08-02 00:54:03 +0600

    first commit

--- index.js
+++ index.js
@@ -2,25 +2,4 @@
         let textElement = document.getElementById("myText");
         textElement.innerText = "Boom! You've unlocked a colorful surprise! Keep clicking to explore more fun!";
         textElement.style.color = "#" + Math.floor(Math.random()*16777215).toString(16); // Random color
-
-        // Get the video container
-        let videoContainer = document.getElementById("videoContainer");
-
-        // Clear any existing video (if the button is clicked multiple times)
-        videoContainer.innerHTML = '';
-
-        // Create a new video element
-        let videoElement = document.createElement("video");
-        videoElement.src =  "https://www.w3schools.com/html/mov_bbb.mp4"; // Replace with your video URL
-        videoElement.controls = true; // Show video controls (play, pause, volume)
-        videoElement.autoplay = true; // Autoplay the video
-        videoElement.loop = true; // Loop the video
-        videoElement.style.width = "100%"; // Make video responsive within its container
-        videoElement.style.maxWidth = "600px"; // Limit max width
-u
-        // Append the video to the container
-        videoContainer.appendChild(videoElement);
-
-        // Make the video container visible
-        videoContainer.style.display = "block";
     }
